5021404350fb24effe46514e4e1aeebcc83ca7ce
feranmi-idowu <idowuferanmi4@gmail.com>
2026-03-02 10:05:51 +0100

    error fixed

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Footer from "./components/Footer";
-import Login from "./components/Login";
+import Footer from "../components/Footer";
+import Login from "../components/Login";
 
 function loginPage () {
   return (
